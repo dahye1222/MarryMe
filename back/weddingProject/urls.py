7a7704599f78dc90d9abf02122a08617c2aa64cf
DAHYE LEE <dahye.lee.1998@gmail.com>
2025-05-28 05:03:58 +0900

    modify urlpatterns

--- back/weddingProject/urls.py
+++ back/weddingProject/urls.py
@@ -38,6 +38,9 @@ urlpatterns = [
     path('api/gold-prices/', include('gold_prices.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+if settings.DEBUG and settings.STATICFILES_DIRS:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+elif not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
